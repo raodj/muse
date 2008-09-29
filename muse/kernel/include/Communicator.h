386f497e545b578d54a66fc05355d55af8a19c17
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2008-09-29 17:38:08 +0000

    Added skeletal classes and headers for Communicator and Scheduler. These classes have not yet been documented. Also added (with ReferencesOfLibs.txt) a list of Heap implementations under the derectory lib/heaps.tar.gz - Mez

---
+++ muse/kernel/include/Communicator.h
@@ -0,0 +1,24 @@
+#ifndef _MUSE_COMMUNICATOR_H_
+#define _MUSE_COMMUNICATOR_H_
+
+using namespace muse;
+class Communicator {
+
+class Event;
+
+public:
+	Communicator();
+	~Communicator();
+
+	void sendEvent(Event *, SimulatorID *);
+
+	void initialize();
+
+	void sendMessage(char *, SimulatorID *);
+
+	void sendMessageToAll(char *); //not sure what this would be usefull for??
+
+	
+};
+
+#endif
---
+++ muse/kernel/src/Communicator.cpp
@@ -0,0 +1,15 @@
+#ifndef _MUSE_COMMUNICATOR_H_
+#include "../include/Communicator.h"
+#endif
+
+void Communicator::Communicator(){}
+
+void Communicator::initialize(){}
+
+void Communicator::sendEvent(Event *, SimulatorID *){}
+
+void Communicator::sendMessage(char *, SimulatorID *){}
+
+void Communicator::sendMessageToAll(char *){}
+
+void Communicator::~Communicator(){}
