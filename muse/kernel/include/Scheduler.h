7513cb1de0dee53dd3d3e5a7c05aa46abf8571b4
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2008-12-05 15:50:35 +0000

    Removed The Time class and we are now using typedef for now. Also added the implementation of the scheduler class!!

--- muse/kernel/include/Scheduler.h
+++ muse/kernel/include/Scheduler.h
@@ -1,23 +1,44 @@
 #ifndef _MUSE_SCHEDULER_H_
 #define _MUSE_SCHEDULER_H_
 
-#include "../../include/Event.h"
+#include "Event.h"
+#include "DataTypes.h"
+//#include "Simulation.h"
+#include <queue>
+#include <map>
 
-
+using namespace std;
 using namespace muse;
+
+//compares delivery times. puts the one with the smaller
+//ahead.
+
+class eventComp
+{
+public:
+  eventComp(){}
+  bool operator() ( Event *lhs,  Event *rhs) const
+  {
+     return (lhs->getReceiveTime() > rhs->getReceiveTime());
+  }
+};
+
+typedef priority_queue <Event*, vector<Event*>, eventComp> EventPQ;
+typedef map<AgentID, EventPQ*> ScheduleMap;
+
 class Scheduler {
 
 public:
 	Scheduler();
 	~Scheduler();
 	
-	bool reset();
-	bool sceduleEvent(Event *);
-	bool sceduleEvents(EventContainer *);
-	EventContainer* getNextEvents();
-	
-	
+	bool scheduleEvent( Event *);
+	bool scheduleEvents( EventContainer *);
+	EventContainer* getNextEvents(const AgentID &);
+    bool addAgentToScheduler(const AgentID &);
+ 
 private:
-	
+    ScheduleMap schedule;
 };
+
 #endif
--- muse/kernel/src/Scheduler.cpp
+++ muse/kernel/src/Scheduler.cpp
@@ -2,15 +2,64 @@
 #include "../include/Scheduler.h"
 #endif
 
-
 Scheduler::Scheduler(){}
 
-EventContainer* Scheduler::getNextEvents(){return NULL;}
+bool
+Scheduler::addAgentToScheduler(const AgentID & agentID){
+    if (schedule[agentID] == NULL) {
+         std::cout << "Created an Entry in the scheduler for Agent: " << agentID << std::endl; 
+        schedule[agentID] = new EventPQ;
+        return true;
+    }
+    return false;
+}
 
-bool Scheduler::reset(){return false;}
+//please make sure to delete the Container when you are done
+EventContainer* Scheduler::getNextEvents(const AgentID & agent){
+    //quick check, if heap empty, then return NULL
+    if (schedule[agent]->empty()) return NULL;
+    
+    //not sure about this, but the user should delete this pointer
+    //when they are done. Maybe we should use Smart pointers here
+    
+    EventContainer *events = new EventContainer;
+    Event *rootEvent = schedule[agent]->top();
+    //std::cout << "Scheduler Size: " << schedule[agent]->size() << std::endl;
+    //std::cout << "Getting Root Event for receive Time: " << rootEvent->getReceiveTime() << std::endl;
 
-bool Scheduler::sceduleEvent(Event * e){return false;}
+    events->insert(rootEvent);
+    schedule[agent]->pop();
+    Event *nextEvent = schedule[agent]->top();
+    ///now lets get all the events to process
+    while(rootEvent->getReceiveTime() == nextEvent->getReceiveTime() ){
+        if (schedule[agent]->empty()) break;
+        //std::cout << "Scheduler Size In Loop: " << schedule[agent]->size() << std::endl;
+        //std::cout << "Getting Event for receive Time: " << nextEvent->getReceiveTime() << std::endl;
+        events->insert(nextEvent);
+        schedule[agent]->pop();
+        nextEvent = schedule[agent]->top();
+    }//end while
+    
+    return events;
+}
 
-bool Scheduler::sceduleEvents(EventContainer * events){return false;}
+bool 
+Scheduler::scheduleEvent( Event *e){
+    //first lets look up the receiver agent
+    std::cout << "Scheduler Added event for agent: "<<e->getReceiverAgentID() << std::endl;
+    //make sure the recevier agent has an entry
+    if (schedule[e->getReceiverAgentID()] == NULL) return false;
+    schedule[e->getReceiverAgentID()]->push(e);
+    return true;
+}
+
+bool 
+Scheduler::scheduleEvents( EventContainer *events){
+    EventContainer::iterator it;
+    for(it=events->begin(); it != events->end(); ++it){
+        schedule[(*it)->getReceiverAgentID()]->push((*it));
+    }//end for
+    return true;
+}
 
 Scheduler::~Scheduler(){}
