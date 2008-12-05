7500802709d307b4530583609f455d69234f5641
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2008-12-05 16:53:59 +0000

    Added the core implementation to the Simulation class.

--- kernel/src/Simulation.cpp
+++ kernel/src/Simulation.cpp
@@ -1,17 +1,96 @@
-#ifndef _MUSE_SIMULATION_H_
-#include "../../include/Simulation.h"
-#endif
-using muse::Simulation;
+#ifndef _MUSE_SIMUALTION_CPP_
+#define _MUSE_SIMUALTION_CPP_
 
-Simulation::Simulation(){}
+#include "Simulation.h"
 
-const muse::AgentContainer* Simulation::getRegisteredAgents(){ return NULL;}
-const muse::SimulatorID Simulation::getSimulatorID(){ return this->myID; }
-const muse::AgentID* Simulation::registerAgent( const muse::Agent* agent) const { return NULL;}
+using namespace muse;
 
-muse::Simulation * Simulation::getSimulator(){ return NULL; }
+const AgentContainer* Simulation::getRegisteredAgents(){ 
+	return allAgents;
+}
 
-void Simulation::start(){}
+const SimulatorID Simulation::getSimulatorID(){ return this->_myID; }
+
+
+bool Simulation::registerAgent(  muse::Agent* agent)  { 
+	//add to the agents list
+    if (scheduler.addAgentToScheduler(agent->getAgentID())){
+        allAgents.insert(agent);
+        return true;
+    }//end if
+    return false;
+}//end registerAgent
+
+
+//TODO: for now the simulator id is default, but needs to get
+//fixed to work with MPI
+Simulation& 
+Simulation::getSimulator(){
+    Time startGVT = 0;SimulatorID myID = 1;
+    static Simulation kernel(startGVT,myID);
+    return kernel;
+}
+
+
+bool 
+ Simulation::scheduleEvent( Event *e){
+     return scheduler.scheduleEvent(e);
+ }
+
+void 
+Simulation::start(){
+    AgentContainer::iterator it;
+    //loop for the initialization
+    for (it=allAgents.begin(); it != allAgents.end(); ++it){
+        (*it)->initialize();
+    }//end for
+    
+    //BIG loop for event processing 
+    EventContainer *events = NULL;
+    //while(true){
+        for (it=allAgents.begin(); it != allAgents.end(); ++it){
+            events = scheduler.getNextEvents((*it)->getAgentID());
+            if (events != NULL){
+                //this means we have events to process
+                (*it)->executeTask(events);
+            }//end if
+            //(*it)->initialize();
+        }//end for
+    //}//end BIG loop
+    //since we are done with the events pointer time to delete
+    delete events;
+    
+    //loop for the finalization
+    for (it=allAgents.begin(); it != allAgents.end(); ++it){
+        (*it)->finalize();
+    }//end for
+}//end start
+
 void Simulation::stop(){}
 
-Simulation::~Simulation(){}
+void 
+Simulation::setStartTime(const Time & startTime){
+    _startTime = startTime;
+}
+
+void 
+Simulation::setStopTime(const Time & stopTime){
+    _endTime = stopTime;
+}
+
+const Time& 
+SimulationKernel::getTime(){
+    return _LGVT;
+}
+
+const Time& 
+SimulationKernel::getStartTime(){
+    return _startTime;
+}
+
+const Time& 
+SimulationKernel::getEndTime(){
+    return _endTime;
+}
+
+#endif
