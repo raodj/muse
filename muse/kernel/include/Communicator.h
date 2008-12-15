b6b59b69adc870f46042fba4d71a56ca8e2f408e
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2008-12-15 03:04:27 +0000

    Prepped the Communicator with documentation and some needed datatypes.

--- muse/kernel/include/Communicator.h
+++ muse/kernel/include/Communicator.h
@@ -1,27 +1,58 @@
 #ifndef _MUSE_COMMUNICATOR_H_
 #define _MUSE_COMMUNICATOR_H_
 
-#include "Simulation.h"
+#include "DataTypes.h"
+#include <map>
 
-namespace muse {
+BEGIN_NAMESPACE(muse);
 class Communicator {
 
 class Event;
 
 public:
+        /** The ctor.
+         */
 	Communicator();
+
+         /** The dtor.
+          */
 	~Communicator();
 
-	void sendEvent(Event *, SimulatorID *);
+         /** The sendEvent method.
+          *
+          * @param Event pointer, this is the event to be sent across the wire.
+          * @see Event
+          */
+	void sendEvent(Event *);
 
+        /** The recvEvent method.
+          *
+          * @return Event pointer, this is the event to be received from the wire.
+          * @see Event
+          */
+	Event * recvEvent();
+
+        /** The initialize method
+         * this is used to sync all communicator and get the agentMap
+         * populated.
+         */
 	void initialize();
 
-	void sendMessage(char *, SimulatorID *);
+	void sendMessage(char *);
 
-	void sendMessageToAll(char *); //not sure what this would be usefull for??
+private:
 
-	
+    /** This is used to map the locations of all agents in the simulation.
+     *  When simulation starts, all simulation kernels, will perform a bcast and
+     *  send all agents that they have. Using the agentID as the key, the communicator
+     *  will be able to know the simulator kernel's ID.
+     *
+     * @see AgentID
+     * @see SimulatorID
+     */
+    map<AgentID, SimulatorID> agentMap;
 };
-}
+
+END_NAMESPACE(muse);
 
 #endif
