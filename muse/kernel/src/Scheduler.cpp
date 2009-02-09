b78266b304694b33705b9cedd01753a2a887f5d5
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2009-02-09 22:57:11 +0000

    check in for the day

--- muse/kernel/src/Scheduler.cpp
+++ muse/kernel/src/Scheduler.cpp
@@ -59,6 +59,10 @@ bool Scheduler::scheduleEvent( Event *e){
 
     //first check if this is a rollback!
     if (e->getReceiveTime() <= agent->LVT){
+        if (e->getSenderAgentID() ==  e->getReceiverAgentID()){
+            cout << "Can't Rollback from Event sent to myself: returning false: " << *e <<endl;
+            return false;
+        }
         cout << "\nDetected a ROLLBACK @ agent: "<<agent->getAgentID() << endl <<endl;
 	//debug info print out
 	cout << "[Scheduler] - Output Queue Size: "<< agent->outputQueue.size() <<endl;
@@ -85,9 +89,9 @@ bool Scheduler::scheduleEvent( Event *e){
 
             if ( (*del_it)->getReceiveTime() >= e->getReceiveTime() &&
                  (*del_it)->getSenderAgentID() == e->getSenderAgentID()){
-                 (*del_it)->makeAntiMessage();
+                //(*del_it)->makeAntiMessage();
                  cout << "---Tagged Anti-message"<<endl;
-                 //agent->eventPQ.remove(del_it.getNode());
+                 agent->eventPQ.remove(del_it.getNode());
             }//end if
          }//end while
     }//end if future anti-message check
