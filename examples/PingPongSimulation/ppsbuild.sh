c6640e3b95abb5506238345edc0f0d0bdbcb6f62
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2009-02-15 22:41:32 +0000

    Added a proper make file to build the ping pong simulation example. In addition removed a couple of delete calls at the end of the example to avoid a memory error.

--- examples/PingPongSimulation/ppsbuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpicxx -DHAVE_CONFIG_H -DDEVELOPER_ASSERTIONS -I../muse/kernel/include -I../muse/include -Wall -g -L../muse/kernel -lmuse -lstdc++ PingPongAgent.cpp PingPongSimulation.cpp -o pp_sim
