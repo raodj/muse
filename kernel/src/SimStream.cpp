ce81b1aeb44c3242f946ea2fab65583ab252c237
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2009-03-02 17:30:49 +0000

    added SimStream as a base class for oSimStream

---
+++ kernel/src/SimStream.cpp
@@ -0,0 +1,11 @@
+#include "SimStream.h"
+using namespace muse;
+
+void
+SimStream::garbageCollect(const Time& gvt){}
+
+void
+SimStream::saveState(const Time& lvt){}
+
+void
+SimStream::rollback(const Time& restored_time){}
