d5099205286e01ee5974e43ff61b09caab9c4131
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2009-03-03 14:00:14 +0000

    Removed generated files from svn archive.

--- muse/examples/RoundRobinSimulation/build.sh
+++ muse/examples/RoundRobinSimulation/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpicxx -DHAVE_CONFIG_H -DDEVELOPER_ASSERTIONS  -I../../include  -g -O0 -L../../kernel -lmuse -lstdc++ RoundRobinAgent.cpp RoundRobinSimulation.cpp -o roundrobinTest
+mpicxx -DHAVE_CONFIG_H -DDEVELOPER_ASSERTIONS  -I../../include  -g -O0 -L../../kernel -lstdc++ RoundRobinAgent.cpp RoundRobinSimulation.cpp -o roundrobinTest -lmuse
