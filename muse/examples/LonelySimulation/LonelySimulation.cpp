b8e3e5b6d71753f58a09d6a6f22f113749dacce3
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2009-03-03 20:44:21 +0000

    Fixed a bug in GVT manager that was causing multiple rounds of GVT to overlap before broadcasts from previous rounds reached various processes. This was causing two different kinds of problems:
       1. In certain cases, the number of cycles could exceed 3.
       2. In certain cases, the GVT notifcations would arrive when process is not
          red.
    
    Fixed this issue by having each process respond to gvt_est_msgs with a 
    gvt_ack_msg  and have the process-rank-0 to wait to receive acks from all the
    other processes before starting the next cycle.
    
    Added makefiles for some of the examples.

--- muse/examples/LonelySimulation/LonelySimulation.cpp
+++ muse/examples/LonelySimulation/LonelySimulation.cpp
@@ -16,6 +16,7 @@
 #include "State.h"
 #include "DataTypes.h"
 #include <math.h>
+#include <cstdlib>
 
 using namespace muse;
 using namespace std;
@@ -76,6 +77,6 @@ int main(int argc, char** argv) {
     //now we finalize the kernel to make sure it cleans up.
     kernel->finalize();
     
-    return (EXIT_SUCCESS);
+    return (0);
 }
 
