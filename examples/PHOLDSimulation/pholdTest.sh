0313008983662a461611faf1c53dbe398a8dcabc
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2009-03-04 17:55:28 +0000

    make files added and bug fix in Agent::scheduleEvent

--- examples/PHOLDSimulation/pholdTest.sh
+++ examples/PHOLDSimulation/pholdTest.sh
@@ -3,7 +3,7 @@
 cd /home/gebremr/research/muse/examples/PHOLDSimulation 
 ulimit -c unlimited
           #<X> <Y> <N> <Delay> <Max Nodes> <Simulation endTime>
-./pholdTest 10   2   10     2       1                 10000 &
+./pholdTest 1  20  10    10       4                 10000 &
 pid=$!
 wait $pid
 status=$?
