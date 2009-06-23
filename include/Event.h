27f5a6792f2a0c93909ddf75cd574ecbaa75eaf2
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2009-06-23 17:02:54 +0000

    Modifed the interface to provide flexiblity to create events of varying sizes. This provides the ability to send and receive events of varying sizes. The API is still not bullet proof.

--- include/Event.h
+++ include/Event.h
@@ -28,6 +28,24 @@
 // Forward declaration for insertion operator for Event
 extern std::ostream& operator<<(ostream&, const muse::Event&);
 
+/** \def CREATE(EventClasName, int size, receiverID, recveTime)
+
+    \brief A convenience macro to facilitate the definition of MUSE
+    API compliant static create method for instantiating events.
+
+    This macro must be used in each class that is derived from this
+    Event class to implement a suitate create method.
+*/
+#define CREATE(EventClassName)                                          \
+    static EventClassName* create(const muse::AgentID receiverID,       \
+                                  const muse::Time recvTime,            \
+                                  const int size = sizeof(EventClassName)) { \
+        EventClassName* event =                                         \
+            reinterpret_cast<EventClassName*>(new char[size]);          \
+        new (event) EventClassName(receiverID, recvTime);               \
+        return event;                                                   \
+    }
+
 BEGIN_NAMESPACE(muse); //begin namespace declaration
 
 /** The base class for all events in a simulation.
@@ -41,143 +59,150 @@ BEGIN_NAMESPACE(muse); //begin namespace declaration
 */
 class Event {
  
-  friend std::ostream& ::operator<<(ostream&, const muse::Event&);
-  friend class Agent;
-  friend class Simulation;
-  friend class Scheduler;
-  friend class GVTManager;
-  friend class Communicator;
-  friend class BinaryHeapWrapper;
+    friend std::ostream& ::operator<<(ostream&, const muse::Event&);
+    friend class Agent;
+    friend class Simulation;
+    friend class Scheduler;
+    friend class GVTManager;
+    friend class Communicator;
+    friend class BinaryHeapWrapper;
   
- public:
-  /** The ctor method.
-      This is the fastest way to create an Event object. The AgentID
-      of the agent receiving the event and the delivery time is the
-      least amount of information needed to be a vaild Event.
+public:
+    // Use MUSE macro to define the create method to isntantiate
+    // this event class.
+    CREATE(Event);
+
+    /** The getSenderAgentID method.
+        @return reference to the sender AgentID.
+        @see AgentID
+    */
+    inline AgentID getSenderAgentID() const { return senderAgentID;}
+
+    /** The getReceiverAgentID method.
+        @return reference to the receiver AgentID.
+        @see AgentID
+    */
+    inline AgentID getReceiverAgentID() const { return receiverAgentID; }
+  
+    /** The getSentTime method.
+        @return reference to the sent time of this event.
+        @see Time
+    */
+    inline  Time getSentTime() const { return sentTime; }
+  
+    /** The getReceiveTime method.
+        @return reference to the receive time of this event.
+    */
+    inline Time getReceiveTime() const { return receiveTime; }
+
+    inline int getReferenceCount() const {return referenceCount;}
+
+protected:
+    /** The ctor method.
+        
+        The constructor is protected to ensure that the user always
+        uses the static CREATE method to create events. The AgentID of
+        the agent receiving the event and the delivery time is the
+        least amount of information needed to be a vaild Event.
+        
+        @param receiverID the id of the receiver agent
+        
+        @param receiveTime this is the time the receiving agent should
+        process this event.
+        
+        @see AgentID
+        @see Time
+    */
+    explicit Event(const AgentID  receiverID,const Time  receiveTime);
+    
+    /** The getColor method.
+        This method must be used to determine the color value
+        associated with this event.  The color value is typically used
+        for GVT computations during simulation.
       
-     
-      @param receiverID the id of the receiver agent
-      @param receiveTime this is the time the receiving agent should process this event.
-
-      @see AgentID
-      @see Time
-  */
-  explicit Event(const AgentID  receiverID,const Time  receiveTime);
-
-  /** The getSenderAgentID method.
-      @return reference to the sender AgentID.
-      @see AgentID
-  */
-  inline AgentID getSenderAgentID() const { return senderAgentID;}
-
-  /** The getReceiverAgentID method.
-      @return reference to the receiver AgentID.
-      @see AgentID
-  */
-  inline AgentID getReceiverAgentID() const { return receiverAgentID; }
+        @return The color value associated with this event.  The color
+        value is always zero or one. Any other value potentially
+        indicates an error.
+    */
+    inline char getColor() const { return color; }
   
-  /** The getSentTime method.
-      @return reference to the sent time of this event.
-      @see Time
-  */
-  inline  Time getSentTime() const { return sentTime; }
-  
-  /** The getReceiveTime method.
-      @return reference to the receive time of this event.
-  */
-  inline Time getReceiveTime() const { return receiveTime; }
-
-  inline int getReferenceCount() const {return referenceCount;}
-
- protected:
-  /** The getColor method.
-      This method must be used to determine the color value
-      associated with this event.  The color value is typically used
-      for GVT computations during simulation.
+    /** Set the color value for this event.
+        This method must be used to set the color value associated
+        with this event.  The color value is typically used for GVT
+        computations during simulation.
       
-      @return The color value associated with this event.  The color
-      value is always zero or one. Any other value potentially
-      indicates an error.
-  */
-  inline char getColor() const { return color; }
-  
-  /** Set the color value for this event.
-      This method must be used to set the color value associated
-      with this event.  The color value is typically used for GVT
-      computations during simulation.
-      
-      @param color The color value associated with this event.
-      The color value is always zero or one. Any other value
-      potentially indicates an error.
-  */
-  void setColor(const char color);
+        @param color The color value associated with this event.
+        The color value is always zero or one. Any other value
+        potentially indicates an error.
+    */
+    void setColor(const char color);
 
 
-  /** The getEventSize method.
-      This method is very important, when it comes time to send an event
-      across the wire via MPI.
+    /** The getEventSize method.
+        This method is very important, when it comes time to send an event
+        across the wire via MPI.
 
-      @note USERS should override this method if event class is inherited and return
-      correct size of the inherited event. For example the ClockEvent would override
-      this and returns sizeof(ClockEvent)
+        @note USERS should override this method if event class is inherited and return
+        correct size of the inherited event. For example the ClockEvent would override
+        this and returns sizeof(ClockEvent)
 
-      @return int, the size of the event.
-   */
-  virtual int getEventSize() const {return sizeof(Event); }
+        @return int, the size of the event.
+    */
+    virtual int getEventSize() const {return sizeof(Event); }
   
-  /** The decreaseReference method.
-      Used for memory management.
-      This method should not be used by users. MUSE usues this to handle memory for you.
-  */
-  void decreaseReference();
+    /** The decreaseReference method.
+        Used for memory management.
+        This method should not be used by users. MUSE usues this to handle memory for you.
+    */
+    void decreaseReference();
   
-  /** The increaseReference method.
-      Used for memory management.
-      This method should not be used by users. MUSE usues this to handle memory for you.
-  */
-  void increaseReference();
+    /** The increaseReference method.
+        Used for memory management.
+        This method should not be used by users. MUSE usues this to handle memory for you.
+    */
+    void increaseReference();
   
-  /** The isAntiMessage method.
-      @return bool True if this event is an anti-message.
-  */
-  inline bool isAntiMessage() const { return antiMessage; }
+    /** The isAntiMessage method.
+        @return bool True if this event is an anti-message.
+    */
+    inline bool isAntiMessage() const { return antiMessage; }
   
-  /** The makeAntiMessage
-      converts this event into an anti-message
-      This method should only be used by muse internal operations.
-  */
-  void makeAntiMessage();
+    /** The makeAntiMessage
+        converts this event into an anti-message
+        This method should only be used by muse internal operations.
+    */
+    void makeAntiMessage();
 
-  /** Only should be used by the agent rollback method.
-   */
-  inline void setReferenceCount(int count) { referenceCount = count;}
+    /** Only should be used by the agent rollback method.
+     */
+    inline void setReferenceCount(int count) { referenceCount = count;}
  
-  /** The dtor.
-      User should not be able to delete events. Also events can only be created
-      in the heap.
-   */
-  ~Event(); 
+    /** The dtor.
+        User should not be able to delete events. Also events can only be created
+        in the heap.
+    */
+    ~Event(); 
   
-  AgentID senderAgentID, receiverAgentID;
-  Time sentTime,receiveTime;
-  //this is used to let the scheduler know that this event is an anti-message
-  bool antiMessage;      
-  //this is for memory managements
-  int referenceCount; 
+    AgentID senderAgentID, receiverAgentID;
+    Time sentTime,receiveTime;
+    //this is used to let the scheduler know that this event is an anti-message
+    bool antiMessage;      
+    //this is for memory managements
+    int referenceCount; 
   
-  /** Instance variable to hold the color of this event for GVT
-      computation.
+    /** Instance variable to hold the color of this event for GVT
+        computation.
       
-      This instance variable is used to hold the color of this event
-      for GVT computation using Mattern's GVT algorithm.  This
-      character can take one of two values: 1 (for red events) and 2
-      (for white events).  This information is set whenever an event
-      is created and then used by the GVT manager to track events
-      for GVT computation.
+        This instance variable is used to hold the color of this event
+        for GVT computation using Mattern's GVT algorithm.  This
+        character can take one of two values: 1 (for red events) and 2
+        (for white events).  This information is set whenever an event
+        is created and then used by the GVT manager to track events
+        for GVT computation.
       
-      @see GVTManager
-  */
-  char color;
+        @see GVTManager
+    */
+    char color;
 
  
 };
