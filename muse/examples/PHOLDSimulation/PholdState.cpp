9cc825a9fa7c14acce53ab79636a9b263794400a
gebremr <gebremr@ec563325-fe46-0410-a1ed-3931b8991857>
2009-07-20 15:57:37 +0000

    adding PholdState to phold example

---
+++ muse/examples/PHOLDSimulation/PholdState.cpp
@@ -0,0 +1,18 @@
+#ifndef _PHOLDSTATE_CPP
+#define _PHOLDSTATE_CPP
+
+#include "PholdState.h"
+
+PholdState::PholdState() : index(0) {}
+
+State*
+PholdState::getClone(){
+    //keep in mind this shallow copy works because there are no complex
+    //pointers that i need to take care of. Just primative datatypes.
+    PholdState* clone = new PholdState();
+    clone->setIndex(this->getIndex());
+    return clone;
+    //Caller should handle deleting the memory when done with State pointer
+}
+
+#endif
