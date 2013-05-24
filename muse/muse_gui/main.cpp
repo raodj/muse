95db3ffc476869d0822ea93f7a4211960a854ecc
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2013-05-24 19:43:32 +0000

    Created a skeleton Qt project for working on MUSE-GUI with the objective that all MUSE-GUI development efforts will use this project and skeleton files (see disclaimer and license at the top of each file) as basis for development.

---
+++ muse/muse_gui/main.cpp
@@ -0,0 +1,56 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+//---------------------------------------------------------------------
+//    ___
+//   /\__\    This file is part of MUSE    <http://www.muse-tools.org/>
+//  /::L_L_
+// /:/L:\__\  Miami   University  Simulation  Environment    (MUSE)  is
+// \/_/:/  /  free software: you can  redistribute it and/or  modify it
+//   /:/  /   under the terms of the GNU  General Public License  (GPL)
+//   \/__/    as published  by  the   Free  Software Foundation, either
+//            version 3 (GPL v3), or  (at your option) a later version.
+//    ___
+//   /\__\    MUSE  is distributed in the hope that it will  be useful,
+//  /:/ _/_   but   WITHOUT  ANY  WARRANTY;  without  even  the IMPLIED
+// /:/_/\__\  WARRANTY of  MERCHANTABILITY  or FITNESS FOR A PARTICULAR
+// \:\/:/  /  PURPOSE.
+//  \::/  /
+//   \/__/    Miami University  and  the MUSE  development team make no
+//            representations  or  warranties  about the suitability of
+//    ___     the software,  either  express  or implied, including but
+//   /\  \    not limited to the implied warranties of merchantability,
+//  /::\  \   fitness  for a  particular  purpose, or non-infringement.
+// /\:\:\__\  Miami  University and  its affiliates shall not be liable
+// \:\:\/__/  for any damages  suffered by the  licensee as a result of
+//  \::/  /   using, modifying,  or distributing  this software  or its
+//   \/__/    derivatives.
+//
+//    ___     By using or  copying  this  Software,  Licensee  agree to
+//   /\  \    abide  by the intellectual  property laws,  and all other
+//  /::\  \   applicable  laws of  the U.S.,  and the terms of the  GNU
+// /::\:\__\  General  Public  License  (version 3).  You  should  have
+// \:\:\/  /  received a  copy of the  GNU General Public License along
+//  \:\/  /   with MUSE.  If not,  you may  download  copies  of GPL V3
+//   \/__/    from <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------
+
+#include "MainWindow.h"
+#include <QApplication>
+#include "Version.h"
+
+int main(int argc, char *argv[]) {
+    // Initialize QT application (initally processes command-line arguments)
+    QApplication app(argc, argv);
+
+    // Create the main window that will contain various tabbed views that
+    // can be reorganized by dragging and dropping tabs.
+    MainWindow mainWindow;
+    mainWindow.show();
+    
+    // Start the main GUI-event processing loop.
+    return app.exec();
+}
+
+#endif
