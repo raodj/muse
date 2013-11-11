39305d88a76801b2dc37b36123fb2619b2acaa78
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2013-11-11 17:35:38 +0000

    Added missing call to mpiexec in the shell script that was causing issues with running multiple processes on multiple nodes.

--- examples/PHOLDSimulation/pholdTestPbs.sh
+++ examples/PHOLDSimulation/pholdTestPbs.sh
@@ -2,14 +2,14 @@
 # Lines beginning with # are comments
 # Only lines beginning #PBS are processed by qsub
 
-#PBS -N hello
+#PBS -N phold_benchmark
 #PBS -l walltime=4:00:00
 #PBS -l mem=40GB
-#PBS -l nodes=4:ppn=8
+#PBS -l nodes=2:ppn=2
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/PHOLDSimulation
-time ./phold 800 800 100 5 20 30
+time mpiexec ./phold 10 10 10 5 4 30
 
 #end of script
 
