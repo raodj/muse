bb1dd6a524b3e90c9a996fe362cc5e2ba8e76187
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2013-11-11 13:44:17 +0000

    Modified the absolute path in the PBS script to use envrionment variables so that the PBS job shell scripts are portable to different users.
    Such portability is an important aspect to keep in mind when developing
    these scripts.

--- examples/PHOLDSimulation/pholdTestPbs.sh
+++ examples/PHOLDSimulation/pholdTestPbs.sh
@@ -8,7 +8,7 @@
 #PBS -l nodes=4:ppn=8
 #PBS -S /bin/bash
 
-cd /home/varadhsa/research/muse/examples/PHOLDSimulation/
+cd $HOME/research/muse/examples/PHOLDSimulation
 time ./phold 800 800 100 5 20 30
 
 #end of script
