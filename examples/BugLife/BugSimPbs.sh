b5c7460c1ecc71db43dfc3f98cbc1c4074e9e405
varadhsa <varadhsa@ec563325-fe46-0410-a1ed-3931b8991857>
2014-02-07 03:43:36 +0000

    New format of command line arguments for bugsim, in batch file

--- examples/BugLife/BugSimPbs.sh
+++ examples/BugLife/BugSimPbs.sh
@@ -3,13 +3,15 @@
 # Only lines beginning #PBS are processed by qsub
 
 #PBS -N buglife_benchmark
-#PBS -l walltime=4:00:00
-#PBS -l mem=40GB
+#PBS -l walltime=1:00:00
+#PBS -l mem=20GB
 #PBS -l nodes=1:ppn=2
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/BugLife
-time ./bugsim 20 20 6 2 30
+#time ./bugsim 20 20 3 1 30
+echo "time mpiexec ./bugsim --cols 10 --rows 10 --bugs 12 --nodes 4 --end 200 ..\n\n"
+time mpiexec ./bugsim --cols 10 --rows 10 --bugs 12 --nodes 4 --end 200
 
 #end of script
 
