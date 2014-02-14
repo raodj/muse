a9d8b53acda1dce93e009503d3d0ee6c351be9bc
higirodj <higirodj@ec563325-fe46-0410-a1ed-3931b8991857>
2014-02-14 20:16:12 +0000

    Looks like mpiexec requires an -n <num_procs> option. Without the option it generates a bizzare error due to some form of incompatibility. Possibly some more investigation into the source of this compatibility may be warranted on the long run.

--- examples/PHOLDSimulation/pholdTestPbs.sh
+++ examples/PHOLDSimulation/pholdTestPbs.sh
@@ -4,13 +4,14 @@
 
 #PBS -N phold_benchmark
 #PBS -l walltime=4:00:00
-#PBS -l mem=170GB
-#PBS -l nodes=8:ppn=8
+#PBS -l mem=120GB
+#PBS -l nodes=2:ppn=4
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/PHOLDSimulation
-echo "time mpiexec ./phold 4000 4000 5 1 40 30 ..\n\n"
-time mpiexec ./phold 4000 4000 5 1 40 30
+echo "time mpiexec ./phold 100 100 5 1 8 30 ..\n\n"
+# ulimit -c unlimited
+time mpiexec -n 8 ./phold 100 100 5 1 8 30
 
 #end of script
 
