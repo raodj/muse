33ecc32473a460d8de9acc8259fa3077fd89c0b5
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2014-03-09 23:59:28 +0000

    A copule of changes to RescueArea:
    1. Removed "using namespace muse;" from the code as it does not belong in
       "good" code.
    
    2. Fixed a logic error where the number of victims was not correctly computed
       when the number of volunteers exceed the `MAX_EVENT_ARRAY_SIZE` (currently
       set to 10) but the victims do not.

--- examples/RescueSim/agents/include/RescueArea.h
+++ examples/RescueSim/agents/include/RescueArea.h
@@ -1,25 +1,48 @@
-#ifndef RescueArea_H
-#define RescueArea_H
+#ifndef RESCUE_AREA_H
+#define RESCUE_AREA_H
 
 #include "Agent.h"
 #include "State.h"
 #include "VolunteerDataTypes.h"
 
-using namespace muse;
-
-class RescueArea : public Agent {
+class RescueArea : public muse::Agent {
 public:
-   RescueArea(AgentID, State *);
-   void initialize() throw (std::exception);
-   void executeTask(const EventContainer*);
-   void finalize();
+    RescueArea(muse::AgentID, muse::State *);
+    void initialize() throw (std::exception);
+    void executeTask(const muse::EventContainer*);
+    void finalize();
+
+protected:
+    /** Convenience method to determine number of entries to be copied
+	into an event.
+
+	This method is a simple convenience method that is used to
+	determine the number of items to be copied into an event.
+	Events have fixed number of items they can contain indicated
+	by MAX_EVENT_ARRAY_SIZE constant.  Consequently, items that
+	exceed this count are dispatched using multiple events.  This
+	method is a convenience method to determine items to be packed
+	into an event.
+
+	\param[in] itemCount The total number of times to be packed
+	into one or more event(s).
+
+	\param[in] startPos The starting position (in the list of
+	items) from where the data is to be copied.  If this value is
+	greater then itemCount then this method returns zero.
+
+	\param[in] maxItems An optional argument that indicates that
+	the number of items that can be packaged into an event.
+    */
+    int eventItemCount(const size_t itemCount, int startPos,
+		       const size_t maxItems = MAX_EVENT_ARRAY_SIZE) const;
 
 private:
     // Helper method to dispatch nearby event
-    void scheduleNearbyEvent(const AgentID,
-                             const std::vector<AgentID>&,
+    void scheduleNearbyEvent(const muse::AgentID,
+                             const std::vector<muse::AgentID>&,
                              const std::vector<coord>&,
                              const bool);
 };
 
-#endif /* RescueArea_H */
+#endif
--- examples/RescueSim/agents/src/RescueArea.cpp
+++ examples/RescueSim/agents/src/RescueArea.cpp
@@ -1,66 +1,73 @@
-#ifndef RescueArea_CPP
-#define RescueArea_CPP
+#ifndef RESCUE_AREA_CPP
+#define RESCUE_AREA_CPP
 
 #include "RescueArea.h"
 #include "RescueAreaState.h"
 #include "UpdatePositionEvent.h"
 #include "UpdateNearbyEvent.h"
 
-RescueArea::RescueArea(AgentID id, State* state) : Agent(id,state) { }
+RescueArea::RescueArea(muse::AgentID id, muse::State* state) :
+    Agent(id,state) { }
 
 void
 RescueArea::initialize() throw (std::exception) { }
 
+int
+RescueArea::eventItemCount(const size_t itemCount, int startPos,
+			   const size_t maxItems) const {
+    return std::max(0, std::min(int(itemCount) - startPos, int(maxItems)));
+}
+
 void
-RescueArea::scheduleNearbyEvent(const AgentID receiver,
-                                const std::vector<AgentID>& nearbyVols,
+RescueArea::scheduleNearbyEvent(const muse::AgentID receiver,
+                                const std::vector<muse::AgentID>& nearbyVols,
                                 const std::vector<coord>& nearbyVics,
                                 const bool isLastCol) {
-   const size_t MaxPos = std::max(nearbyVols.size(), nearbyVics.size());
-   for(size_t startPos = 0; startPos < MaxPos; startPos += MAX_EVENT_ARRAY_SIZE) {
-      const int numVols = std::min(nearbyVols.size() - startPos,
-                                   MAX_EVENT_ARRAY_SIZE);
-      const int numVics = std::min(nearbyVics.size() - startPos,
-                                   MAX_EVENT_ARRAY_SIZE);
-      UpdateNearbyEvent* updateNear = UpdateNearbyEvent::create(receiver, 
-                                                                getTime() + 0.01);
-      updateNear->setNearbyVols(&nearbyVols[startPos], numVols);
-      updateNear->setNearbyVics(&nearbyVics[startPos], numVics);
-      scheduleEvent(updateNear);
-   }
-   if(isLastCol) {
-      UpdateNearbyEvent* updateNear = UpdateNearbyEvent::create(receiver, 
-                                                                getTime() + 0.01);
-      updateNear->setMessageFinal();
-      scheduleEvent(updateNear);
-   }
+    const int MaxPos = std::max(nearbyVols.size(), nearbyVics.size());
+    for(int startPos = 0; startPos < MaxPos; startPos += MAX_EVENT_ARRAY_SIZE) {
+	const int numVols = eventItemCount(nearbyVols.size(), startPos);
+	const int numVics = eventItemCount(nearbyVics.size(), startPos);
+	UpdateNearbyEvent* updateNear =
+	    UpdateNearbyEvent::create(receiver, getTime() + 0.01);
+	updateNear->setNearbyVols(&nearbyVols[startPos], numVols);
+	updateNear->setNearbyVics(&nearbyVics[startPos], numVics);
+	scheduleEvent(updateNear);
+    }
+    if(isLastCol) {
+	UpdateNearbyEvent* updateNear = UpdateNearbyEvent::create(receiver, 
+								  getTime() + 0.01);
+	updateNear->setMessageFinal();
+	scheduleEvent(updateNear);
+    }
 }
 
 void
-RescueArea::executeTask(const EventContainer* events){
-   UpdatePositionEvent *curEvent = NULL;
-   for(EventContainer::const_iterator it = events->begin();
-                                                       it != events->end(); it++) {
-      RescueEvent* const current_event = static_cast<RescueEvent*>((*it));
-      RescueAreaState* const my_state  = static_cast<RescueAreaState*>(getState());
-      if(current_event->getEventType() == UpdatePositionVolunteer) {
-         curEvent = static_cast<UpdatePositionEvent*>(current_event);
-         const coord loc      = curEvent->getCurrentLocation();
-         const AgentID sender = current_event->getSenderAgentID();
-         my_state->updateVolunteerPosition(sender, loc);
-         scheduleNearbyEvent(current_event->getSenderAgentID(),
-                             my_state->getNearbyVolunteers(sender),
-                             my_state->getNearbyVictims(sender),
-                             (my_state->getColID() == loc.second/AREA_COL_WIDTH));
-      }
-      else if(current_event->getEventType() == UpdatePositionVictim) {
-         curEvent = static_cast<UpdatePositionEvent*>(current_event);
-         my_state->addVictim(curEvent->getCurrentLocation());
-      }
-   }
+RescueArea::executeTask(const muse::EventContainer* events){
+    UpdatePositionEvent *curEvent = NULL;
+    for(muse::EventContainer::const_iterator it = events->begin();
+	it != events->end(); it++) {
+	RescueEvent* const current_event = static_cast<RescueEvent*>((*it));
+	RescueAreaState* const my_state  = static_cast<RescueAreaState*>(getState());
+	if(current_event->getEventType() == UpdatePositionVolunteer) {
+	    curEvent = static_cast<UpdatePositionEvent*>(current_event);
+	    const coord loc            = curEvent->getCurrentLocation();
+	    const muse::AgentID sender = current_event->getSenderAgentID();
+	    my_state->updateVolunteerPosition(sender, loc);
+	    scheduleNearbyEvent(current_event->getSenderAgentID(),
+				my_state->getNearbyVolunteers(sender),
+				my_state->getNearbyVictims(sender),
+				(my_state->getColID() == loc.second/AREA_COL_WIDTH));
+	}
+	else if(current_event->getEventType() == UpdatePositionVictim) {
+	    curEvent = static_cast<UpdatePositionEvent*>(current_event);
+	    my_state->addVictim(curEvent->getCurrentLocation());
+	}
+    }
 }
 
 void
-RescueArea::finalize() { }
+RescueArea::finalize() {
+    // Nothing else to be done for now.
+}
 
-#endif /* RescueArea_CPP */
+#endif
