5449627a8f833593c21cad700e54aa3cf292f6aa
varadhsa <varadhsa@ec563325-fe46-0410-a1ed-3931b8991857>
2014-04-20 05:11:04 +0000

    Clean up of Space class. Moved code for each event out of executeTask().

--- muse/examples/BugLife/src/Space.h
+++ muse/examples/BugLife/src/Space.h
@@ -1,14 +1,46 @@
-/* 
-  This is a template to quickly get going with development
-  The usuless task of setting up the bases classes is done for you.
-  Just change names around and get to the good part.
- */
-
 #ifndef _SPACE_H
 #define	_SPACE_H
 
+//---------------------------------------------------------------------
+//    ___
+//   /\__\    This file is part of MUSE    <http://www.muse-tools.org/>
+//  /::L_L_
+// /:/L:\__\  Miami   University  Simulation  Environment    (MUSE)  is
+// \/_/:/  /  free software: you can  redistribute it and/or  modify it
+//   /:/  /   under the terms of the GNU  General Public License  (GPL)
+//   \/__/    as published  by  the   Free  Software Foundation, either
+//            version 3 (GPL v3), or  (at your option) a later version.
+//    ___
+//   /\__\    MUSE  is distributed in the hope that it will  be useful,
+//  /:/ _/_   but   WITHOUT  ANY  WARRANTY;  without  even  the IMPLIED
+// /:/_/\__\  WARRANTY of  MERCHANTABILITY  or FITNESS FOR A PARTICULAR
+// \:\/:/  /  PURPOSE.
+//  \::/  /
+//   \/__/    Miami University  and  the MUSE  development team make no
+//            representations  or  warranties  about the suitability of
+//    ___     the software,  either  express  or implied, including but
+//   /\  \    not limited to the implied warranties of merchantability,
+//  /::\  \   fitness  for a  particular  purpose, or non-infringement.
+// /\:\:\__\  Miami  University and  its affiliates shall not be liable
+// \:\:\/__/  for any damages  suffered by the  licensee as a result of
+//  \::/  /   using, modifying,  or distributing  this software  or its
+//   \/__/    derivatives.
+//
+//    ___     By using or  copying  this  Software,  Licensee  agree to
+//   /\  \    abide  by the intellectual  property laws,  and all other
+//  /::\  \   applicable  laws of  the U.S.,  and the terms of the  GNU
+// /::\:\__\  General  Public  License  (version 3).  You  should  have
+// \:\:\/  /  received a  copy of the  GNU General Public License along
+//  \:\/  /   with MUSE.  If not,  you may  download  copies  of GPL V3
+//   \/__/    from <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------
+
 #include "Agent.h"
 #include "State.h"
+#include "BugEvent.h"
+#include "SpaceState.h"
+
 using namespace muse;
 
 class Space : public Agent {
@@ -22,7 +54,17 @@ public:
 
     void finalize();
 
+protected:
+    void executeMoveIn(BugEvent* current_event, SpaceState* my_state);
+	
+    void executeMoveOut(BugEvent* current_event, SpaceState* my_state);
+	
+    void executeGrow(BugEvent* current_event, SpaceState* my_state);
+	
+    void executeEat(BugEvent* current_event, SpaceState* my_state);
+	
+    void executeScout(BugEvent* current_event, SpaceState* my_state);
+	
 };
 
 #endif	/* _SPACE_H */
-
