de4badf8c46748e0366d30d90971b373dab0cfb5
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2014-04-29 17:48:34 +0000

    General code clean-up:
         * Cleaned namespace pollution (aka use of "using namespace" directive)
         * General formatting/spacing issues
    
    The header file is still missing API documentation; that needs to be added.

--- muse/examples/BugLife/src/Space.cpp
+++ muse/examples/BugLife/src/Space.cpp
@@ -1,5 +1,5 @@
-#ifndef _SPACE_CPP
-#define	_SPACE_CPP
+#ifndef SPACE_CPP
+#define	SPACE_CPP
 
 //---------------------------------------------------------------------
 //    ___
@@ -43,126 +43,121 @@
 #include "Eat.h"
 #include "Scout.h"
 
-using namespace std;
-
 #define NO_BUG  -1u
 
-Space::Space(AgentID id, State* state) : Agent(id,state){}
+Space::Space(muse::AgentID id, muse::State* state) : Agent(id, state) {
+    // Nothing else to be done in the constructor
+}
 
-void Space::initialize() throw (std::exception)
-{
+void
+Space::initialize() throw (std::exception) {
+    // Nothing else to be done in this method
+} 
 
-}//end initialize
-
-void Space::executeTask(const EventContainer* events)
-{
+void
+Space::executeTask(const EventContainer* events) {
     EventContainer::const_iterator it = events->begin();
-    
-    for (; it != events->end(); it++){
-        BugEvent * current_event = static_cast<BugEvent*>((*it));
-        SpaceState * my_state = static_cast<SpaceState*>(getState());
-        //we use a switch on the event type
-        switch(current_event->getEventType())
-		{
-        case MOVE_IN:
-	    {
-			executeMoveIn(current_event, my_state);
+    for (; (it != events->end()); it++){
+        BugEvent *current_event = static_cast<BugEvent*>(*it);
+        SpaceState *my_state    = static_cast<SpaceState*>(getState());
+        // We use a switch on the event type
+        switch(current_event->getEventType()) {
+	case MOVE_IN:
+	    executeMoveIn(current_event, my_state);
+	    break;
+	case MOVE_OUT:
+	    executeMoveOut(current_event, my_state);
             break;
-		}
-        case MOVE_OUT:
-	    {
-			executeMoveOut(current_event, my_state);
-            break;
-		}
         case EAT:
-	    {
-			executeEat(current_event, my_state);
+	    executeEat(current_event, my_state);
             break;
-		}
         case SCOUT:
-		{
-			executeScout(current_event, my_state);
-			break;
-		}
-        } //end switch
-    }//end for
-}//end executeTask()
+	    executeScout(current_event, my_state);
+	    break;
+	default:
+	    std::cerr << "Error: Unhandled event event type encountered "
+		      << "in Space agent.\n";
+        } // End switch
+    } // End for
+} // End executeTask()
 
-void Space::finalize()
-{
-    
-}//end finalize
-
-void Space::executeMoveIn(BugEvent* current_event, SpaceState* my_state)
-{
-	MoveIn     * move_in  = static_cast<MoveIn*>(current_event);
-	if ( my_state->getBugID() == NO_BUG)
-	{
-		//means we are not holding a bug 
-		my_state->setBugID(move_in->getSenderAgentID());
-		MoveIn * move = MoveIn::create(move_in->getSenderAgentID() , getTime()+1, MOVE_IN);
-		move->canBugMoveIn = true;
-		scheduleEvent(move);
-	} else
-	{ 
-		//means we are full and no bugs allowed
-		MoveIn * move = MoveIn::create(move_in->getSenderAgentID(), getTime()+1, MOVE_IN);
-		move->canBugMoveIn = false; //false by default, but just to be clear to reader
-		scheduleEvent(move);
-	}
+void
+Space::finalize() {
+    // Nothing else to be done here.
 }
 
-void Space::executeMoveOut(BugEvent* current_event, SpaceState* my_state)
-{
-	//we dont need to static cast to MoveOut because we all need info from base class
-	if (my_state->getBugID() == current_event->getSenderAgentID()){
-		//this means that current bug living here wants to move out.
-		my_state->setBugID(NO_BUG);
-		MoveOut * move_out = MoveOut::create(current_event->getSenderAgentID(), getTime()+1, MOVE_OUT);
-		scheduleEvent(move_out);
-	}
+void
+Space::executeMoveIn(BugEvent* current_event, SpaceState* my_state) {
+    MoveIn     * move_in  = static_cast<MoveIn*>(current_event);
+    if ( my_state->getBugID() == NO_BUG) {
+	//means we are not holding a bug 
+	my_state->setBugID(move_in->getSenderAgentID());
+	MoveIn * move = MoveIn::create(move_in->getSenderAgentID() ,
+				       getTime()+1);
+	move->canBugMoveIn = true;
+	scheduleEvent(move);
+    } else { 
+	//means we are full and no bugs allowed
+	MoveIn * move = MoveIn::create(move_in->getSenderAgentID(),
+				       getTime()+1);
+	move->canBugMoveIn = false;
+	scheduleEvent(move);
+    }
 }
 
-void Space::executeGrow(BugEvent* current_event, SpaceState* my_state)
-{
+void
+Space::executeMoveOut(BugEvent* current_event, SpaceState* my_state) {
+    // We dont need to static cast to MoveOut because we all need info
+    // from base class
+    if (my_state->getBugID() == current_event->getSenderAgentID()){
+	//this means that current bug living here wants to move out.
+	my_state->setBugID(NO_BUG);
+	MoveOut * move_out = MoveOut::create(current_event->getSenderAgentID(),
+					     getTime()+1);
+	scheduleEvent(move_out);
+    }
 }
 
-void Space::executeEat(BugEvent* current_event, SpaceState* my_state)
-{
-	//ok, we need to check how much food is in this space
-	//we cast to get the eat amount, this is how much food there was in the space.
-	Eat * eat_event           = static_cast<Eat*>(current_event);
-	//making these vars for readability
-	int food_here       = my_state->getFood();
-	int bug_eat_amount  = eat_event->eatAmount;
-	//if we can provide the full eat amount then we do else we give what we have left
-	Eat * eat   = Eat::create(current_event->getSenderAgentID(), getTime()+1, EAT);
-	if (food_here > bug_eat_amount)
-	{
-		//good news for the bug it gets what it wants
-		eat->setEatAmount(bug_eat_amount);
-		//now we need to reduce the food count in our state
-		my_state->setFood( (food_here-bug_eat_amount) );
-	} else
-	{
-		//this means that we dont have enough for the bug, so we send him what we have
-		//and set our food count to zero
-		eat->setEatAmount(food_here);
-		//now we need to reduce the food count in our state
-		my_state->setFood(0);
-	}
-	//now we tell the bug
-	scheduleEvent(eat);
+void
+Space::executeGrow(BugEvent*, SpaceState*) {
+    // This method does not have any tasks to perform in this agent.
 }
 
-void Space::executeScout(BugEvent* current_event, SpaceState* my_state)
-{
-	executeScout(current_event, my_state);
-	//ok here we send the bug info about the space.
-	//cout << "space got scout"<<endl;
-	Scout * scout_reply = Scout::create(current_event->getSenderAgentID(), getTime()+1);
-	scout_reply->foodCount = my_state->getFood();
-	scheduleEvent(scout_reply);
+void
+Space::executeEat(BugEvent* current_event, SpaceState* my_state) {
+    // Ok, we need to check how much food is in this space We cast to
+    // get the eat amount, this is how much food there was in the
+    // space.
+    Eat* eat_event     = static_cast<Eat*>(current_event);
+    //making these vars for readability
+    int food_here      = my_state->getFood();
+    int bug_eat_amount = eat_event->eatAmount;
+    // If we can provide the full eat amount then we do else we give
+    // what we have left
+    Eat * eat   = Eat::create(current_event->getSenderAgentID(), getTime()+1, EAT);
+    if (food_here > bug_eat_amount) {
+	// Good news for the bug it gets what it wants
+	eat->setEatAmount(bug_eat_amount);
+	// Now we need to reduce the food count in our state
+	my_state->setFood(food_here - bug_eat_amount);
+    } else {
+	// This means that we dont have enough for the bug, so we send
+	// it what we have and set our food count to zero
+	eat->setEatAmount(food_here);
+	// Now we need to reduce the food count in our state
+	my_state->setFood(0);
+    }
+    // Now we tell the bug
+    scheduleEvent(eat);
+}
+
+void
+Space::executeScout(BugEvent* current_event, SpaceState* my_state) {
+    // Ok here we send the bug info about the space.
+    Scout* scout_reply = Scout::create(current_event->getSenderAgentID(),
+					getTime() + 1);
+    scout_reply->foodCount = my_state->getFood();
+    scheduleEvent(scout_reply);
 }
 
 #endif
