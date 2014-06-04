fee8605515b410ee3c0fc01b341e02d5b3148e10
blasedd <blasedd@ec563325-fe46-0410-a1ed-3931b8991857>
2014-06-04 21:56:39 +0000

    Fixed the reference to the banner picture for the wizard.

--- muse_gui/core/MUSEWizard.cpp
+++ muse_gui/core/MUSEWizard.cpp
@@ -1,7 +1,7 @@
 #include "MUSEWizard.h"
 
 MUSEWizard::MUSEWizard(QWidget *parent) {
-    setPixmap(QWizard::BannerPixmap, QPixmap(":/images/logo/MuseLogo.png"));
+    setPixmap(QWizard::BannerPixmap, QPixmap(":/images/logo/MuseLogo.jpg"));
     //setPixmap(QWizard::BackgroundPixmap, QPixmap(":/images/logo/MuseLogo.png"));
 
 
