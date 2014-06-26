334e1681eb6da7963c1cf319d0f88ffb6be1c4e3
varadhsa <varadhsa@ec563325-fe46-0410-a1ed-3931b8991857>
2014-06-26 16:13:00 +0000

    Changed the PBS job script file, so that it is easier to configure the arguments before scheduling the jobs.\n 1. Moved the argument to a variable, thereby avaoiding multiple editting.\n 2. Added ulimit, for the core dump file.\n 3. Reorganized the print statements.

--- muse/examples/PHOLDSimulation/pholdTestPbs.sh
+++ muse/examples/PHOLDSimulation/pholdTestPbs.sh
@@ -2,16 +2,17 @@
 # Lines beginning with # are comments
 # Only lines beginning #PBS are processed by qsub
 
-#PBS -N phold_benchmark
-#PBS -l walltime=4:00:00
+#PBS -N $HOME/pholdLogFiles/phold_benchmark
+#PBS -l walltime=5:00:00
 #PBS -l mem=120GB
-#PBS -l nodes=2:ppn=4
+#PBS -l nodes=1:ppn=1
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/PHOLDSimulation
-echo "time mpiexec ./phold --rows 100 --cols 100 --eventsPerAgent 5 --delay 1 --computeNodes 8 --simEndTime 30 ..\n\n"
+args="--rows 200 --cols 200 --eventsPerAgent 1000 --delay 5 --computeNodes 1 --simEndTime 100"
+echo -e "time mpiexec ./phold $args \n"
+echo -e "\n #######################"
 # ulimit -c unlimited
-time mpiexec -n 8 ./phold --rows 100 --cols 100 --eventsPerAgent 5 --delay 1 --computeNodes 8 --simEndTime 30 
+time mpiexec ./phold $args
 
 #end of script
-
