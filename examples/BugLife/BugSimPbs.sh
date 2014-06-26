ed254b7397a149c40c3c83bd8832e1b4d9acd2ee
varadhsa <varadhsa@ec563325-fe46-0410-a1ed-3931b8991857>
2014-06-26 16:13:00 +0000

    Changed the PBS job script file, so that it is easier to configure the arguments before scheduling the jobs.\n 1. Moved the argument to a variable, thereby avaoiding multiple editting.\n 2. Added ulimit, for the core dump file.\n 3. Reorganized the print statements.

--- examples/BugLife/BugSimPbs.sh
+++ examples/BugLife/BugSimPbs.sh
@@ -3,15 +3,17 @@
 # Only lines beginning #PBS are processed by qsub
 
 #PBS -N buglife_benchmark
-#PBS -l walltime=1:00:00
-#PBS -l mem=20GB
-#PBS -l nodes=1:ppn=2
+#PBS -l walltime=4:00:00
+#PBS -l mem=200GB
+#PBS -l nodes=2:ppn=4
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/BugLife
-#time ./bugsim 20 20 3 1 30
-echo "time mpiexec ./bugsim --cols 10 --rows 10 --bugs 12 --nodes 4 --end 200 ..\n\n"
-time mpiexec ./bugsim --cols 10 --rows 10 --bugs 12 --nodes 4 --end 200
+#ulimit -c unlimited
+#echo "unlimit \n"
+args="--cols 7000 --rows 7000 --bugs 22000000 --nodes 8 --end 700000"
+echo -e "time mpiexec ./bugsim $args ..\n\n"
+echo -e "\n ############################"
+time mpiexec ./bugsim $args
 
 #end of script
-
