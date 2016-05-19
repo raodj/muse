95f937995398ff5b6275c78fa09c633943404f21
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2016-05-19 19:16:56 +0000

    General clean-up of the code base --
    * Removed "using namespace" from both the header and source files
    * Removed passing-in state to agent. instead agents decide on the state they would like to use.
    * Added some more doxygen style documentation to the code

--- muse/examples/PingPongSimulation/PingPongAgent.cpp
+++ muse/examples/PingPongSimulation/PingPongAgent.cpp
@@ -1,49 +1,40 @@
-#ifndef _PINGPONGAGENT_CPP
-#define	_PINGPONGAGENT_CPP
+#ifndef PINGPONGAGENT_CPP
+#define	PINGPONGAGENT_CPP
 
+#include <iostream>
 #include "PingPongAgent.h"
 #include "Event.h"
-#include <iostream>
 
-using namespace std;
-
-PingPongAgent::PingPongAgent(AgentID& id, State* state) : Agent(id,state) {}
-
-void
-PingPongAgent::initialize() throw (std::exception){
-    
-   //this is a custom pingpong simulation, so we can assume only two agents!
-    //the first agent is to start the ping
-    if (getAgentID() == 0){
-       
-        Event * e = Event::create(AgentID(1),getTime()+1);
-        this->scheduleEvent(e);
-        //cout << "Sent ---> PING" << endl;
-    }//end if
-}//end initialize
+PingPongAgent::PingPongAgent(const muse::AgentID& id) : 
+    Agent(id, new muse::State()) {
+    // Nothing else to be done in the constructor
+}
 
 void
-PingPongAgent::executeTask(const EventContainer* events){
-    cout << "PingPong Agent [";cout <<getAgentID();cout << "] @ LVT: ";cout << this->getTime() << endl;
+PingPongAgent::initialize() throw (std::exception) {
+    // This is a custom ping pong simulation, so we can assume only two agents!
+    // the first agent is to start the ping
+    if (getAgentID() == 0) {
+        muse::Event* e = muse::Event::create(muse::AgentID(1), getTime() + 1);
+        scheduleEvent(e);
+    }  //end if
+}  //end initialize
+
+void
+PingPongAgent::executeTask(const muse::EventContainer* events) {
+    std::cout << "PingPong Agent [" << getAgentID() << "] @ LVT: "
+              << getTime() << std::endl;
    
     if (!events->empty()){
-        
-        Event * e = Event::create( (getAgentID()+1)%2 ,getTime()+1);
-        this->scheduleEvent(e);
-        /* if (getAgentID() == 1){
-            cout << "Got  ---> PING" << endl;
-            cout << "Sent ---> PONG" << endl;
-        }else{
-            cout << "Got  ---> PONG" << endl;
-            cout << "Sent ---> PING" << endl;
-            }*/
+        muse::Event * e = muse::Event::create((getAgentID() + 1) % 2, getTime() + 1);
+        scheduleEvent(e);
     }
-}//end executeTask
+}  //end executeTask
 
 void
 PingPongAgent::finalize() {
-    //cleanup and shutdown
-  
-}//end finalize
+    //cleanup and shutdown  
+}  //end finalize
+
 #endif //_PINGPONGAGENT_CPP
 
--- muse/examples/PingPongSimulation/PingPongAgent.h
+++ muse/examples/PingPongSimulation/PingPongAgent.h
@@ -5,23 +5,49 @@
  * Created on January 15, 2009, 11:04 PM
  */
 
-#ifndef _PINPONGGAGENT_H
-#define	_PINPONGGAGENT_H
+#ifndef PING_PONG_AGENT_H
+#define	PING_PONG_AGENT_H
 
 #include "Agent.h"
 #include "State.h"
-using namespace muse;
 
-class PingPongAgent : public Agent {
-
+class PingPongAgent : public muse::Agent {
 public:
-    PingPongAgent(AgentID &, State *);
+    /**
+     * Constructor to create a ping pong agent.
+     * 
+     * @param agentID The unique ID to assign to this agent. This
+     * value is set once when an agent is instantiated and is never
+     * changed during the life time of this agent. Note that a default
+     * (but invalid) value of -1 is set to ease creation of an vector
+     * of agents.
+     */
+    PingPongAgent(const muse::AgentID &agentID = -1);
 
-    void initialize() throw (std::exception);
+    /**
+     * The initialize method for this object. The initialize method
+     * is called only once when an object is created. This method 
+     * essentially generates the first event in the simulation 
+     * only on agent with ID 0.
+     */
+    void initialize() throw (std::exception) override;
 
-    void executeTask(const EventContainer* events);
+    /**
+     * This method is called each time this agent has an event
+     * to process. This method essentially creates a new event
+     * and dispatches it to a different agent in the simulation.
+     * 
+     * @param events The list of events to process. This list 
+     * will contain at least one event for this agent to process.
+     */
+    void executeTask(const muse::EventContainer* events) override;
 
-    void finalize();
+    /**
+     * This method is called once just before the simulation 
+     * completes. This method currently just prints a simple
+     * message.
+     */
+    void finalize() override;
 
 };
 
