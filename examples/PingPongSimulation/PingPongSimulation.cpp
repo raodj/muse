af9eef4958491d046bf17b9aeaffa7e3f98ce207
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2016-05-19 19:16:56 +0000

    General clean-up of the code base --
    * Removed "using namespace" from both the header and source files
    * Removed passing-in state to agent. instead agents decide on the state they would like to use.
    * Added some more doxygen style documentation to the code

--- examples/PingPongSimulation/PingPongSimulation.cpp
+++ examples/PingPongSimulation/PingPongSimulation.cpp
@@ -6,60 +6,50 @@
  */
 
 #include <iostream>
+#include <cstdlib>
 #include "PingPongAgent.h"
 #include "Simulation.h"
-#include "State.h"
 #include "DataTypes.h"
-#include <cstdlib>
 
-using namespace muse;
-using namespace std;
-
-#define PING_KERNEL 0
-#define PONG_KERNEL 1
+// Named constants to make code more readable
+const int PING_KERNEL = 0;
+const int PONG_KERNEL = 1;
 
 /*
- * 
+ * The main method that setups the simulation and runs a
+ * simple ping-pong simulation with exactly 2 agents.
  */
 int main(int argc, char** argv) {
-  //cout << "Ping Pong Simulation Example via MUSE API\n\n" << endl;
-    //first get simulation kernel instance to work with
-    Simulation * kernel = Simulation::getSimulator();
-    
-    //now lets initialize the kernel
-    kernel->initialize(argc,argv);
+    // First get simulation kernel instance to work with
+    muse::Simulation* kernel = muse::Simulation::getSimulator();
+    // Now lets initialize the kernel (and MPI)
+    kernel->initialize(argc, argv);
 
-    State *pingpong_state = new State();
-    
-    //now SPMD way of coding comes into play here!!!
-    //NOTE:: here we register the agents (players) to different simulation
-    //       kernels!!!!!
-    SimulatorID kernel_id = kernel->getSimulatorID();
-    //cout << "Kernel ID: " << kernel_id << endl;
-    PingPongAgent *pp_agent = NULL;
+    // now SPMD way of coding comes into play here!!!
+    // NOTE:: here we register the agents (players) to different simulation
+    //        kernels!!!!!
+    muse::SimulatorID kernel_id = kernel->getSimulatorID();
     if (kernel_id == PING_KERNEL){
-        AgentID my_id = 0;
-        pp_agent = new PingPongAgent(my_id,pingpong_state);
+        PingPongAgent *pp_agent = new PingPongAgent(PING_KERNEL);
         kernel->registerAgent(pp_agent);
-    }else if (kernel_id == PONG_KERNEL){
-        AgentID my_id = 1;
-        pp_agent = new PingPongAgent(my_id,pingpong_state);
+    } else {
+        PingPongAgent *pp_agent = new PingPongAgent(PONG_KERNEL);
         kernel->registerAgent(pp_agent);
-    }//end if-elseif
+    }
 
-    //we set the start and end time of the simulation here
-    Time start=0, end= (argc >= 2) ? atof(argv[1]) : 10; 
+    // we set the start and end time of the simulation here
+    muse::Time start = 0, end = (argc >= 2) ? std::stod(argv[1]) : 10;
     kernel->setStartTime(start);
     kernel->setStopTime(end);
 
-    //we finally start the ping pong simulation here!!
+    // we finally start the ping pong simulation here!!
     kernel->start();
 
-    //now we finalize the kernel to make sure it cleans up.
+    // now we finalize the kernel to make sure it cleans up.
     kernel->finalize();
 
-    //lets not forget to clean up the mez we make :-)
-    //we don't have to worry about the kernel it takes care itself!
+    // lets not forget to clean up the mez we make :-)
+    // we don't have to worry about the kernel it takes care itself!
     return 0;
 }
 
