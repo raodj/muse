58fa17931d63f6cdb5357a956784eb79201b8946
raodm <raodm@ec563325-fe46-0410-a1ed-3931b8991857>
2016-12-28 03:05:21 +0000

    When comparing scheduler queue (specifically heap2Q and ladderQ) using PHOLD, there are 8 parameters that could influence the outcome, namely:
        ['--rows']="10 100"
        ['--cols']="10 100"
        ['--delay']="0 100"
        ['--delay-distrib']="1 5"
        ['--eventsPerAgent']="1 20"
        ['--granularity']="0 50"
        ['--simEndTime']="50 500"
        ['--imbalance%']="0 100"
    
    However, analyzing and reporting all 8 parameters with multiple
    parallel simulation configuration is rather cumbersome, particularly
    in a meaningful way in a conference paper.
    
    Consequently, this is an attempt at reducing the number of parameters
    using Generalized Sensitivity Analysis (GSA).  Currently, only source
    and scripts are added to the repository.  I have done basic testing
    and plotting of test data.
    
    Next step is to run at least 10,000 short runs of PHOLD to see if we
    can clearly identify the most significant/influential set of
    parameters to PHOLD.

---
+++ Thesis/higirodj/parameter_analysis/gen_gsa_data.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+#PBS -S /bin/bash
+#PBS -j eo
+
+# This is a shell script that can either be run standalone or via PBS
+# as a PBS script.  This shell script performs the following tasks:
+#
+#  1. Uses a given set of parameters and range of values.
+#  2. Generates a set of Sobol random numbers
+#  3. Translates Sobol numbers to parameter range
+#  4. Runs PHOLD simulations using 2 different scheduler queues
+#  5. Appends values to results files.
+
+# The simulation executable to be used.  Typically it is a symbolic
+# link to the actual executable.
+SIM_EXEC="./phold"
+
+# The default values for the two types of queues we are working with
+QUEUE1_DEFAULT="ladderQ"
+QUEUE2_DEFAULT="heap2tQ"
+
+# The list of command-line parameters to be explored for GSA.  These
+# values change from simulation-to-simulation.  Entries are in the
+# form: cmd-line-arg="min max"
+
+declare -A CmdLineParams
+CmdLineParams=( ['--rows']="10 100"
+                ['--cols']="10 100"
+                ['--delay']="0 100"
+                ['--delay-distrib']="1 5"
+                ['--eventsPerAgent']="1 20"
+                ['--granularity']="0 50"
+                ['--simEndTime']="50 500"
+                ['--imbalance%']="0 100"
+              )
+
+# ------ Typically you should not have to modify values below this line. -----
+
+# The Sobol random number generation program.
+SOBOL_GEN="./Sobol"
+
+# Convenience function print parameters and ranges set
+function printParamRanges() {
+    # Print the command-line parameters for verifications
+    for param in "${!CmdLineParams[@]}"
+    do
+        paramRange=( ${CmdLineParams[$param]} )
+        echo "$param ${paramRange[@]}"
+    done    
+}
+
+# Convenience function to check and set variables using command-line
+# arguments.  The command line parameters to the script are passed-in
+# as the parameters to this function from main
+function parseArgs() {
+    while [[ $# > 0 ]]
+    do
+        key="$1"
+        shift
+        case $key in
+            --reps)
+                NUM_REPS=$1
+                shift
+                ;;
+            --skip)
+                SKIP_VALS=$1
+                shift
+                ;;
+            --outfile)
+                OUT_FILE="$1"
+                shift
+                ;;
+            --queue1)
+                QUEUE1="$1"
+                shift
+                ;;
+            --queue2)
+                QUEUE2="$1"
+                shift
+                ;;
+            --tmpdir)
+                TMPDIR="$1"
+                shift
+                ;;
+            *)
+                echo "Unknown option $1 encountered"
+                showHelpAndExit
+                ;;
+        esac
+    done
+}
+
+function checkParams() {
+    if [ -z "$NUM_REPS" ]; then
+        echo "Value of NUM_REPS (--reps) not set."
+        return 1
+    fi
+    if [ -z "$SKIP_VALS" ]; then
+        echo "Value for SKIP_VALS (--skip) not set."
+        return 1
+    fi
+    if [ -z "$OUT_FILE" ]; then
+        echo "Output log file OUT_FILE (--outfile) not set."
+        return 1
+    fi
+    if [ -z "$QUEUE1" ]; then
+        QUEUE1=${QUEUE1_DEFAULT}
+    fi
+    if [ -z "$QUEUE2" ]; then
+        QUEUE2=${QUEUE2_DEFAULT}
+    fi
+    if [ -z "$TMPDIR" ]; then
+        TMPDIR="/tmp"
+    fi
+    # All variables look good
+    return 0
+}
+
+# Convenience function to convert a Sobol random number to value
+# associated with a single parameter.
+#   $1: Parameter key
+#   $2: Sobol random number
+function param2CmdLine() {
+    # Create aliases for parameter to streamline script.
+    local key="$1"
+    local rndNum="$2"
+    # Get min & max values for the specified parameter
+    local paramRange=( ${CmdLineParams[$param]} )
+    # Extract min and max to make formula below easier to read
+    local min=${paramRange[0]}
+    local max=${paramRange[1]}
+    local range=$(( max - min ))
+    # Scale the rndNum to min and max values
+    local paramVal=`echo "$min + $rndNum * $range" | bc -l | cut -d'.' -f1`
+    # If the parameter is a percentage convert it to 0 to 1.0 value
+    if [ "${key: -1}" == "%" ]; then
+        paramVal=`echo "$paramVal / 100.0" | bc -l`
+        # Remove traling % from key.
+        key="${key:0:-1}"
+    fi
+    # Echo the parameter and its value
+    echo "$key $paramVal"
+}
+
+# Convenience function to convert Sobol random numbers to
+# corresponding command-line argument values.
+#    $* : A set of Sobol random numbers (each in range 0 to 1.0)
+#
+function toCmdLine() {
+    # The set of sobol random numbers to work with.
+    local rndNums=( $* )
+    # The command-line being built by this function
+    cmdLine=""
+    # Convert each Sobol random number to corresponding parameter value.
+    local idx=0
+    for param in "${!CmdLineParams[@]}"
+    do
+        local paramVal=`param2CmdLine "$param" ${rndNums[$idx]}`
+        cmdLine="${cmdLine} $paramVal"
+        # Skip to next Sobol random number
+        idx=$(( idx + 1 ))
+    done
+    # Echo the command-line back to the caller
+    echo "$cmdLine"
+}
+
+# Helper method to add a simple header to the output CSV file to make
+# the data easy to interpret
+function checkAddHeader() {
+    if [ ! -f "$OUT_FILE" ]; then
+        # Print fixed timestamp column first
+        echo -n "# 'Timestamp', " >> "$OUT_FILE"
+        # Print the command-line parameters for verifications
+        for param in "${!CmdLineParams[@]}"
+        do
+            paramRange=( ${CmdLineParams[$param]} )
+            echo -n "'$param ${paramRange[@]}', " >> "$OUT_FILE"
+        done    
+        # Add fixed timings for the 2 queues
+        echo "${QUEUE1}_timing_secs, ${QUEUE2}_timing_secs" >> "$OUT_FILE"
+    fi
+}
+
+# Convenience function to log timings and parameter values from
+# command-line arguments.
+#  $1: Timing for queue1
+#  $2: Timing for queue2
+#  $3: Command-line arguments supplied to the simulation.
+function logGSAdata() {
+    local time1=$1
+    local time2=$2
+    local params=( $3 )
+    local logLine=`date`
+    # First print a comma-separated list of parameter values only
+    # (don't print name of command-line argument)
+    for i in `seq 1 2 ${#params[@]}`
+    do
+        logLine="${logLine}, ${params[$i]}"
+    done
+    # Echo log entry with timings to output log file.
+    echo "${logLine}, ${time1}, ${time2}" >> "$OUT_FILE"
+}
+
+# Helper function to run 2 sequential simulations with 2 different
+# queues and record the elapsed time.
+function compare_queues() {
+    local cmdLine="$1"
+    local pid="$$"
+    local suffix="${pid}_timing.txt"
+    echo "Running simulations for: $cmdLine..."
+    # Repeat simulations for 3 times and average the values.
+    for i in `seq 0 2`
+    do
+        # Set output files to record timings without mixing outputs
+        local queue1_log="${TMPDIR}/${QUEUE1}_${i}_${suffix}"
+        local queue2_log="${TMPDIR}/${QUEUE2}_${i}_${suffix}"
+        # Run the two simulation in parallel (assume 2 cores are allocated)
+        /usr/bin/time -p -o "${queue1_log}" $SIM_EXEC $cmdLine --scheduler-queue $QUEUE1 > /dev/null &
+        /usr/bin/time -p -o "${queue2_log}" $SIM_EXEC $cmdLine --scheduler-queue $QUEUE2 > /dev/null &
+    done
+    # Wait for all 6 processes to finish
+    wait
+    # Extract timings for the 6 runs
+    local q1_time=( 0 0 0 )
+    local q2_time=( 0 0 0 )
+    for i in `seq 0 2`
+    do
+        # Set output files to record timings without mixing outputs
+        local queue1_log="${TMPDIR}/${QUEUE1}_${i}_${suffix}"
+        local queue2_log="${TMPDIR}/${QUEUE2}_${i}_${suffix}"
+        # Extract timings for the 2 queues
+        q1_time[$i]=`head -1 "${queue1_log}" | cut -d" " -f2`
+        q2_time[$i]=`head -1 "${queue2_log}" | cut -d" " -f2`
+    done
+    # Compute average time from the 3 runs
+    local queue1_time=`echo "( ${q1_time[0]} + ${q1_time[1]} + ${q1_time[2]} ) / 3" | bc -l`
+    local queue2_time=`echo "( ${q2_time[0]} + ${q2_time[1]} + ${q2_time[2]} ) / 3" | bc -l`
+    # Round timings to 3 decimals for convenience
+    queue1_time=`printf "%.3f" ${queue1_time}`
+    queue2_time=`printf "%.3f" ${queue2_time}`
+    # Print timings for checking
+    echo "    ${QUEUE1} timings: $queue1_time ( ${q1_time[@]} )"
+    echo "    ${QUEUE2} timings: $queue2_time ( ${q2_time[@]} )"
+    # Log the parameters and the timings observed
+    logGSAdata "${queue1_time}" "${queue2_time}" "${cmdLine}"
+}
+
+# The main function that coordinates tasks in this script.
+function main() {
+    # First process all command-line arguments and setup global variables
+    parseArgs $*
+    # Next check to ensure we have all the necessary variables setup
+    checkParams
+    if [ $? -ne 0 ]; then
+        # Some varibles were not set (or are not valid)
+        exit 1
+    fi
+    echo "TMPDIR=$TMPDIR"
+    # Check add header to output file (if it does not have one already)
+    checkAddHeader
+    # Print parameters and ranges for cross verifications
+    printParamRanges
+    # Now generate NUM_REPS Sobol numbers and simulate each configuration.
+    local paramCount=${#CmdLineParams[@]}
+    ${SOBOL_GEN} ${paramCount} ${NUM_REPS} ${SKIP_VALS} | \
+        while IFS='' read -r rndNums || [[ -n "$rndNums" ]]; do
+            # echo "Sobol random numbers: $rndNums"
+            local cmdLine=`toCmdLine ${rndNums}`
+            # echo "cmdLine = $cmdLine"
+            # Record the time for 2 queues using helper function
+            compare_queues "$cmdLine"
+        done
+}
+
+# Run the main function
+main $*
+
+# End of script
---
+++ Thesis/higirodj/parameter_analysis/plot_gsa_anal.gnu
@@ -0,0 +1,54 @@
+# Gnuplot script to plot sensitivity analysis results from a given file
+# NOTE: ---- This script requires Gnuplot version 4.6 ----
+
+# Run this script as:
+# gnuplot -e 'suffix="./tmp/gsa1"' -e 'output="gsa1_anal.pdf"' plot_gsa_anal.gnu
+#
+
+# Default directory to find logs (if one is not explicitly set)
+if (!exists('output')) {
+  output = suffix . "_gsa_anal.pdf"
+}
+# Setup default error log file if one is not set
+csvLog = suffix . "_gsa_anal.csv"
+
+# Set output and plot formats
+set terminal pdfcairo enhanced truecolor size 4in,3in font ",12"
+set output output
+
+set xtics rotate by 270
+set yrange[0:*]
+
+# Function to get last line from SA error log
+genData(x) = system(sprintf("./extract_gsa_for_plot.sh %s %s", suffix, csvLog))
+# Extract data from logs into another file for convenient plotting
+print genData(1)
+
+# Helper function to extract metric and error for a given parameter index
+getData(idx, col) = system(sprintf("grep '^%d,' %s | cut -d',' -f%d", idx, csvLog, col))+0
+
+# Colors for the different parameters plotted by this script
+# ColorList="#8dd3c7 #b8860b #928bc1 #fb8072 #80b1d3 #fdb462 #b3de69 #f556a8 #4d4dff #bc80bd #ccebc5"
+ColorList="9294791 12092939 9604033 16482418 8434131 16626786 11787881 16078504 5066239 12353725 13429701"
+getColor(c) = word(ColorList, int(c) + 1)+0
+
+# Draw the 95% CI bars to see overlaps between metrics
+do for [idx=0:7] {
+    metric = getData(idx, 4)
+    ci = getData(idx, 5)
+    set object rect from graph 0, first (metric+ci) to graph 1, first (metric-ci) fc rgb getColor(idx) fillstyle transparent solid 0.15 noborder back
+}
+
+# Set box properties
+set boxwidth 0.5
+set style fill transparent solid 0.75 border lc rgb '#aaaaaa'
+# set style histogram errorbars linewidth 2
+set bars front
+
+# Plot the data generated from the logs
+set datafile separator ","
+plot csvLog using 1:4:(getColor($1)):xtic(3) with boxes lc rgb variable  notitle,\
+     csvLog using 1:4:5 with yerrorbars lt 7 ps 0.1 lc rgb '#333333' lw 4 notitle,\
+     csvLog using ($1+0.25):($4+0.0175):6 with labels rotate by 90 notitle
+
+# End of script
