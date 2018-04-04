4377ae8153c5f32185fb71a429a68dbac34793c7
deperomm <deperomm@ec563325-fe46-0410-a1ed-3931b8991857>
2018-04-04 19:38:40 +0000

    Linden Jonsson based lock free priority queue fully implemented
    Definitely still room for optimization, but preliminary testing validates 1. stability of the queue under high contention with many operations and 2. significant speed up with threading vs sequential (without optimizations enabled)
    
    Next steps will be incorporating queue into Queue Robustness Benchmark to start collecting data, and ultimately incorporate into 3tHeap and hopefully have time to collect benchmark data through muse mpi-mt-shm at which point the technical implementation of my thesis work will be complete.

--- kernel/include/mpi-mt-shm/LockFreePQ.h
+++ kernel/include/mpi-mt-shm/LockFreePQ.h
@@ -25,18 +25,24 @@
 //---------------------------------------------------------------------------
 
 #include <stdlib.h>
-#include <inttypes.h>
+#include <inttypes.h>  // additional int types, specifically uintptr_t
+#include <climits>     // numeric type limits i.e. ULONG_MAX
+#include <mutex>
+#include <deque>
+#include <atomic>
 
 #define KEY_NULL 0
 #define NUM_LEVELS 32
 // todo(deperomm): make this dynamic, paper says should = thread count
 #define MAX_OFFSET 8
 // Key values for first/last dummy nodes
-#define KEY_MIN ( 0UL)
-#define KEY_MAX (~1UL)
+#define KEY_MIN (0)
+#define KEY_MAX (ULONG_MAX)
+// Used to initially set thread_id to an invalid value
+#define THREAD_ID_NULL (UINT_MAX)
 
 typedef unsigned long pkey_t;
-typedef char          *pval_t;
+typedef void          *pval_t;
 
 //Node type, the structure for a single node in the queue
 struct Node_t {
@@ -74,8 +80,17 @@ class LockFreePQ {
 public:
     /**
      * Constructor and destructor, initialize the PQ and finalize it
+     * 
+     * Responsible for setting initial state and clearing memory
      */
     LockFreePQ();
+    
+    /**
+     * Cleans up the queue
+     * 
+     * This operation is NOT thread safe, and should only be called after 
+     * all operations are done with the queue.
+     */
     ~LockFreePQ();
     
     /**
@@ -101,9 +116,11 @@ public:
      * 
      * Also validates that skip links are correct for all nodes
      * 
+     * ** Note: This method is not thread safe
+     * 
      * prints to std::cout buffer
      */
-    void prettyPrint();
+    void print();
     
 private:
     
@@ -139,16 +156,115 @@ private:
     static Node_t* allocNode();
     
     /**
-     * Releases a node from memory
+     * Marks a node as ready for deletion and deletes nodes when safe to do so
      * 
-     * eventually would like this to be handled by a garbage collector
+     * Because threads could still be operating on memory that is ready for
+     * deletion, must wait until all threads have left critical sections before
+     * physically deleting the memory.
+     * 
+     * Nodes that are freed should be totally disconnected from the queue,
+     * meaning any threads that newly enters a critical section will not ever
+     * reach the to-be-freed node. 
      * 
      * @param n - Node_t, the node being marked as no longer needed by the queue
      */
-    static void freeNode(Node_t *n) {
-//        delete *n; not necessary because no destructor for Node_t?
-        free(n);
-    }
+    void freeNode(Node_t *n);
+    
+    /**
+     * Alerts the garbage collector that a thread has entered a critical section
+     * 
+     * When called for first time on a thread, also initializes garabage
+     * collection for that thread
+     */
+    void enterCritical();
+    
+    /**
+     * Alerts garbage collection that this thread has left a critical section
+     * 
+     * Once all threads have left their critical sections, we move forward
+     * one epoch. Nodes that have waited at least a full epoch since being
+     * released are guarenteed to no longer be accessed by any thread
+     */
+    void exitCritical();
+    
+    /**
+     * Goes through the process of changing epoch and garbage collecting
+     * 
+     * This thread should only be called by a single thread exclusively
+     * and therefore must be surrounded by a lock
+     */
+    void garbageCollectAndChangeEpoch();
+    
+    /**
+     * When a new thread accesses the queue, it is assigned an id for garbage
+     * collection purposes
+     * 
+     * thread_id is always a unique power of two, as it is used for bitwise ops
+     * to toggle overall thread state
+     * 
+     * For example, the 3rd thread in the system would get thread_id = 0b100
+     */
+    thread_local static size_t thread_id;
+    size_t *lastId = new size_t(0);        // used to distribute IDs to threads
+    
+    /**
+     * bitmap which represents which threads are acitvely in critical sections
+     * 
+     * Each bit represents a thread actively in a critical section, when a 
+     * thread leaves its critical section it flips its bit back to 0
+     * 
+     * For example, if state = 0110 then two threads are actively in crit secs
+     */
+    std::atomic_uint *thread_curr_state = new std::atomic_uint;
+    
+    /**
+     * bitmap which represents threads that were active when epoch last switched
+     * 
+     * This is specifically set after the moment the epoch switched to ensure
+     * that even if new threads entered and previously active threads exited
+     * since the moment we switched, we guarantee that when this is zero, all
+     * threads that were active at the moment we switched have since exited.
+     */
+    std::atomic_uint *thread_epoch_state = new std::atomic_uint;
+    
+    /**
+     * For garbage collection, any new node that is being marked for deletion is
+     * added to this vector. These nodes are very likely not safe to delete
+     * as other threads could still use them.
+     */
+    std::deque<Node_t*> *pendingDeallocs = new std::deque<Node_t*>;
+    
+    /**
+     * For garbage collection, when we switch epochs (all threads since last
+     * epoch change have left their critical sections), this vector is safe
+     * to free (since it has been at least a full epoch since this vector was
+     * populated, meaning every node was released during the previous epoch). 
+     * 
+     * Every epoch, this queue is processed to free memory and then populated
+     * with the contents of pendingDeallocs.
+     */
+    std::deque<Node_t*> *waitingDeallocs = new std::deque<Node_t*>;
+    
+    /**
+     * Finalizes the entire garbage collection system for queue tear down
+     * 
+     * Clears all pending and waiting deallocations that were removed from the 
+     * queue but not physically deleted yet
+     * 
+     * This should only be called when tearing down the queue and is NOT
+     * thread safe.
+     */
+    void finalizeDeallocs();
+    
+    /**
+     * a mutex to ensure safe insertion into pendingDeallocs queue
+     */
+    std::mutex freeMutex;
+    
+    /**
+     * a mutex to ensure only one thread changes epoch at a time
+     */
+    std::mutex epochMutex;
     
     // variables and pointers
     int     maxOffset;
@@ -156,8 +272,12 @@ private:
     Node_t  *tail;
     
     // Used for linear conguential generator (LCG) for rand levels of skip list
-    static unsigned int  lcg_rand;
+    // todo(deperomm): better rand value generator
+    thread_local static unsigned int lcg_rand;
 };
+
+//g++ -std=c++11 -g -Wall -Wextra -I ../../include -pthread -fsanitize=thread -fPIE -pie LockFreePQ.cpp -o test
+
 
 #endif /* LOCK_FREE_PQ_H */
 
