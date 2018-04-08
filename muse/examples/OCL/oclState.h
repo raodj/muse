13220b830bd4e91200cd09525f12bd5969e91ec3
rothhl <rothhl@ec563325-fe46-0410-a1ed-3931b8991857>
2018-04-08 19:41:14 +0000

    Added ssa and flag to specify the number concurent agents sent to the gpu.

--- muse/examples/OCL/oclState.h
+++ muse/examples/OCL/oclState.h
@@ -1,15 +1,16 @@
 #ifndef MUSE_OCLSTATE_H
 #define MUSE_OCLSTATE_H
 #include "State.h"
+#include <iostream>
+#include <memory>
 BEGIN_NAMESPACE(muse);
 
 class oclState : public muse::State {
     friend class oclSimulation;
     friend class OCLAgent;
         int compartments;
-        float* values;
+        std::unique_ptr<float[]> values;
         oclState(int compartmentNum, float population, float exp);
-        ~oclState();
 };
 END_NAMESPACE(muse);
 
