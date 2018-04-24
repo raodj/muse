27bb5788924f94f4d34f5fbc032580377b42efcc
rothhl <rothhl@ec563325-fe46-0410-a1ed-3931b8991857>
2018-04-24 23:18:55 +0000

    All base ocl classes have been moved to the kernel. Now there is only a synthetic simulation and agent class in the synth folder. I will be adding an ebola example tomorrow as well.

---
+++ examples/OCL/synth/synthSimulation.h
@@ -0,0 +1,66 @@
+#ifndef MUSE_EXAMPLES_OCL_SYNTHSIMULATION_H
+#define MUSE_EXAMPLES_OCL_SYNTHSIMULATION_H
+//---------------------------------------------------------------------------
+//
+// Copyright (c) Miami University, Oxford, OHIO.
+// All rights reserved.
+//
+// Miami University (MU) makes no representations or warranties about
+// the suitability of the software, either express or implied,
+// including but not limited to the implied warranties of
+// merchantability, fitness for a particular purpose, or
+// non-infringement.  MU shall not be liable for any damages suffered
+// by licensee as a result of using, result of using, modifying or
+// distributing this software or its derivatives.
+//
+// By using or copying this Software, Licensee agrees to abide by the
+// intellectual property laws, and all other applicable laws of the
+// U.S., and the terms of this license.
+//
+// Authors: Harrison Roth          rothhl@miamioh.edu
+//          Dhananjai M. Rao       raodm@miamioh.edu
+//
+//---------------------------------------------------------------------------
+
+#include "oclSimulation.h"
+#include "oclScheduler.h"
+#include "synthAgent.h"
+#include "oclState.h"
+#include "AgentPQ.h"
+#include <vector>
+
+class synthSimulation {
+    public:
+
+        synthSimulation();
+
+        /*
+         Set up start and stop time for simulation, finish setup before
+         * starting simulation including calling createAgents
+         * Then calls start
+         * and finalizes simulation
+         */
+//        void simulate();
+
+        /*
+         * Create a set of agents and register them with the simulation 
+         * This function uses simulation class variables to determine
+         * specifics, like total number of agents.
+         */
+        void createAgents();
+
+        synthSimulation(int& argc, char* argv[]);
+        
+        bool ode;
+        bool oclAvailable;
+        int compartments;
+        int rows;
+        int cols;
+        int popSize;
+        int expSize;
+        float step;
+        int stopTime;
+};
+
+#endif /* SYNTHSIMULATION_H */
+
