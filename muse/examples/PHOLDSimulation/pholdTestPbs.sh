f9e9a5aeda551c0d75e82d05dd465c727fac3fed
bentoncl <bentoncl@ec563325-fe46-0410-a1ed-3931b8991857>
2019-11-03 23:04:37 +0000

    Testing scripts for LSTM polling policy. Current scripts generate experiments and perform statistical analysis.

--- muse/examples/PHOLDSimulation/pholdTestPbs.sh
+++ muse/examples/PHOLDSimulation/pholdTestPbs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Lines beginning with # are comments
-# Only lines beginning #PBS are processed by qsub
 
 #PBS -N $HOME/pholdLogFiles/phold_benchmark
 #PBS -l walltime=1:00:00
@@ -9,10 +7,12 @@
 #PBS -S /bin/bash
 
 cd $HOME/research/muse/examples/PHOLDSimulation
+
 args="--rows 200 --cols 200 --eventsPerAgent 1000 --delay 5 --simEndTime 100"
+
 echo -e "time mpiexec ./phold $args \n"
 echo -e "\n #######################"
-# ulimit -c unlimited
+
 time mpiexec ./phold $args
 
 #end of script
